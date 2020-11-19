bbe9c21ffbf863915b05879363c85f52b1a21015
YUTAKA-KOMATSU <73815134+YUTAKA-KOMATSU@users.noreply.github.com>
2020-11-19 16:35:04 +0900

    ユーザー管理機能の指摘箇所修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :birth_date, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-
+  with_options presence: true do
+    validates :nickname
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :birth_date
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
